df9969469a0a61bda4a814bdebe60192bcd36fe7
NVNMR TUS3001 <ge54vec@mytum.de>
2024-11-11 18:40:48 +0100

    Add initial Kinetix camera capture function

--- kinetix_camera_sensor.py
+++ kinetix_camera_sensor.py
@@ -1,1 +1,16 @@
- 
+ from pyvcam import pvc
+from pyvcam.camera import Camera
+
+# Initialize the PVCAM library
+pvc.init_pvcam()
+
+# Open the camera
+cam = Camera()
+cam.open()
+
+# Check if camera is open and ready
+if cam.is_open():
+    print("Kinetix camera initialized successfully.")
+else:
+    print("Failed to initialize the Kinetix camera.")
+
